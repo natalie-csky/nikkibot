43785dff62325e91161239fb35471e760a5a0522
natalie-csky <cerven0@protonmail.com>
2024-01-31 11:01:13 +0100

    add logging

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
+import logging
 import discord
+
+TOKEN = "MTIwMjE2NzAyOTY2NzIwNTE0MQ.GcDc5x.6Eb7cZ7Xln8Qg7P74-twUev7SFm2jxFr6aoljE"
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 
 @client.event
 async def on_ready():
@@ -20,12 +24,4 @@ async def on_message(message):
         await message.channel.send("Hello!")
 
 
-client.run("MTIwMjE2NzAyOTY2NzIwNTE0MQ.GcDc5x.6Eb7cZ7Xln8Qg7P74-twUev7SFm2jxFr6aoljE")
-
-
-# def main() -> int:
-#     return 0
-
-
-# if __name__ == "__main__":
-#     main()
+client.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
