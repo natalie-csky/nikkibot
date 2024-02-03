1ee307676565c75715b9258f25a75fa6a43d3741
natalie-csky <cerven0@protonmail.com>
2024-02-03 19:28:23 +0100

    shortpush

--- bot.py
+++ bot.py
@@ -21,6 +21,9 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.author.guild.name == "Doomertrefpunkt":
+        return
+
     if message.content.startswith("Good girl") or message.content.startswith("good girl"):
         await message.channel.send(":3")
     
