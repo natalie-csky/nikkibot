5621c698665275934ed36b189a1134b0f891cda2
natalie-csky <cerven0@protonmail.com>
2024-02-04 11:48:47 +0100

    shortpush

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ ch = discord.abc.Messageable
 PREFIX = "!Nikki, "
 TOKEN: str
 with open("token", encoding="utf-8") as f:
-    print("reading the token")
     TOKEN = f.read()
 
 intents = discord.Intents.default()
