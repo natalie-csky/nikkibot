b6475ae973057dfb2abba490386839751f2e58e0
natalie-csky <cerven0@protonmail.com>
2024-02-04 09:14:18 +0100

    shortpush

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ async def on_message(message):
         channel = await message.channel.send("test")
     
     if message.content.startswith(PREFIX + "print"):
-        print("test")
+        channel = await message.channel.send("test")
 
 
 client.run(TOKEN)
