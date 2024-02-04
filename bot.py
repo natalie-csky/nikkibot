f4997f41b4736af52efd36b16a49bcbaae093a5e
natalie-csky <cerven0@protonmail.com>
2024-02-04 11:27:25 +0100

    shortpush

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
 import discord
 
+msg = discord.Message
+ch = discord.abc.MessageableChannel
+
 TOKEN = "MTIwMjE2NzAyOTY2NzIwNTE0MQ.GcDc5x.6Eb7cZ7Xln8Qg7P74-twUev7SFm2jxFr6aoljE"
+PREFIX = "!Nikki, "
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = discord.Client(intents=intents)
 
-PREFIX = "!Nikki, "
 
 @client.event
 async def on_ready() -> None:
@@ -15,39 +17,50 @@ async def on_ready() -> None:
 
 
 @client.event
-async def on_message(message: discord.Message) -> None:
+async def on_message(message: msg) -> None:
 
     if message.author == client.user:
         return
 
+    await in_any_guild(message)
+
     if message.guild.name == "Doomertreffpunkt":
         await in_doomertreffpunkt(message)
-    
+
     if message.guild.name == "Geheimlabor":
         await in_geheimlabor(message)
-    
-    
-async def in_doomertreffpunkt(message: discord.Message) -> None:
-    if not (message.channel.name == "botausbeutung" and message.channel.category.name == "chefetage"):
-            return
-
-    if message.content.casefold().startswith("Good girl"):
-        await message.channel.send(":3")
-    
-    if message.content.find(":3") != -1:
-        await message.channel.send(":3")
 
 
-async def in_geheimlabor(message: discord.Message) -> None:
-    if message.content.startswith(PREFIX + "sende DM an"):
-        arguments = message.content.removeprefix(PREFIX + "sende DM an")
-        await message.channel.send(arguments)
-    
+async def in_any_guild(message: msg) -> None:
+
     if message.content.casefold().startswith("good girl"):
-        await message.channel.send(":3")
+        await CMD.say_neko_smile(message.channel)
+
+    if message.content.find(":3") != -1:
+        await CMD.say_neko_smile(message.channel)
 
 
-# def cmd_say_
+async def in_doomertreffpunkt(message: msg) -> None:
+    if not (message.channel.id == 1115389541696667879 and message.channel.category.id == 1113691175803695124):
+        return
+
+
+async def in_geheimlabor(message: msg) -> None:
+    if message.content.startswith(PREFIX + "sende DM an"):
+        argument = message.content.removeprefix(PREFIX + "sende DM an")
+        await CMD.send_dm_all(argument)
 
 
 client.run(TOKEN)
+
+
+class CMD:
+
+    @staticmethod
+    async def say_neko_smile(channel: ch) -> None:
+        channel.send(":3")
+
+
+    @staticmethod
+    async def send_dm_all(argument: msg) -> None:
+        msg.channel.send(argument)
