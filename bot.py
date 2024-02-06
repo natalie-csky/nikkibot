be34d4c22238b025d5db7dbe999edd894421ef40
natalie-csky <cerven0@protonmail.com>
2024-02-06 06:02:21 +0100

    shortpush

--- bot.py
+++ bot.py
@@ -137,8 +137,10 @@ class Command:
 					message.content
 				)
 			await self.channel.send(
-				"Sicher, dass du folgende Nachricht an {user} per DM senden willst?" +
-				"Test"
+				"""Sicher, dass du folgende Nachricht an {user} per DM senden willst?
+				Nachricht:
+				{message}
+				""".format(user=self.to_user, message=message.content)
 			)
 
 
