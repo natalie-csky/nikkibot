f7db420ca0cde4ff6bda8875adf2ad9aeabeb40e
natalie-csky <cerven0@protonmail.com>
2024-02-09 09:45:42 +0100

    shortpush

--- main.py
+++ main.py
@@ -12,26 +12,38 @@ async def wait() -> None:
 	await asyncio.sleep(10)
 
 
+def log_to_file() -> None:
+	f = open("logs/logs.txt", "a")
+	f.write("\n")
+	for log in bot.dm_logs:
+		f.write(log)
+		f.write("\n")
+	f.close()
+
+
+def log_to_webpage() -> None:
+	sanitized_logs: list[str] = []
+	cleaner = Cleaner(tags=ALLOWED_TAGS, strip=True)
+	for log in bot.dm_logs:
+		sanitized_logs.append(cleaner.clean(log))
+	SFTPClient(SFTPClient.Actions.SEND_TO_FILE, "doom_de/user_logs/logs.html", sanitized_logs)
+
+
 async def main() -> None:
 	bot_thread = threading.Thread(target=bot.run, daemon=True)
 	bot_thread.start()
+
 	while True:
 		await wait()
-		f = open("logs/logs.txt", "a")
-		for log in bot.dm_logs:
-			f.write(log)
-			f.write("\n")
-		f.close()
 
-		if len(bot.dm_logs) > 0:
-			sanitized_logs: list[str] = []
+		if not len(bot.dm_logs):
+			continue
 
-			cleaner = Cleaner(tags=ALLOWED_TAGS, strip=True)
-			for log in bot.dm_logs:
-				sanitized_logs.append(cleaner.clean(log))
-			print("sending logs to webpage")
-			SFTPClient(SFTPClient.Actions.SEND_TO_FILE, "doom_de/user_logs/logs.html", sanitized_logs)
-			bot.dm_logs.clear()
+		print("logging")
+		log_to_file()
+		log_to_webpage()
+
+		bot.dm_logs.clear()
 
 
 if __name__ == "__main__":
