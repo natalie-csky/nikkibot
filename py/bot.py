34f0997c56cf73225f07a35c092766360417753e
natalie-csky <cerven0@protonmail.com>
2024-02-10 09:36:29 +0100

    shortpush

--- py/bot.py
+++ py/bot.py
@@ -325,8 +325,7 @@ async def send_wat(message: Message) -> None:
 	await message.channel.send(random_unvalid_response)
 
 async def query_messages(channel_id: int, limit: int) -> list[Message]:
-	# voicechannel, stagechannel, textchannel, thread
-	channel = client.get_channel(channel_id)
+	channel = cast(TextableChannel, client.fetch_channel(channel_id))
 	return [message async for message in channel.history(limit=limit)]
 
 def get_normalized_probability_weights() -> list[float]:
