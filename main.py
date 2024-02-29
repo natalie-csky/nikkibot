2bc5e28fc81842137e786fa37dfb8d0e7781e7ad
natalie-csky <cerven0@protonmail.com>
2024-02-29 22:34:29 +0100

    shortpush

--- main.py
+++ main.py
@@ -48,19 +48,6 @@ def query_messages(messages: list[Message]) -> None:
 def get_code() -> None:
 	pass
 
-# def exchange_code(code: str):
-# 	data = {
-# 		'grant_type': 'authorization_code',
-# 		'code': code,
-# 		'redirect_uri': REDIRECT_URI
-# 	}
-# 	headers = {
-# 		'Content-Type': 'application/x-www-form-urlencoded'
-# 	}
-# 	r = requests.post('%s/oauth2/token' % API_ENDPOINT, data=data, headers=headers, auth=(CLIENT_ID, CLIENT_SECRET))
-# 	r.raise_for_status()
-# 	return r.json()
-
 
 # noinspection PyUnreachableCode
 async def main() -> None:
